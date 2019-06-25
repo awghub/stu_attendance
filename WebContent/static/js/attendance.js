73895e96b578ee7d8240956bacfef79ffbdbf4b9
IAMAWG <m15528265897@163.com>
2019-06-25 15:07:18 +0800

    添加教师信息初始化及退出登录(均为示例接口)

--- WebContent/static/js/attendance.js
+++ WebContent/static/js/attendance.js
@@ -1,12 +1,58 @@
 $(function() {
+
+    // 初始化教师的头像及姓名
+	alert(document.URL);
+	var tid = document.URL.split('?')[1].split("=")[1];
+    initTeacherInfo(tid);
+    
     // 初始化加载导航依赖(element)模块
-    layui.use('element', function(){
-      var element = layui.element;
+    layui.use('element', function() {
+        var element = layui.element;
 
-      //监听导航点击
-      element.on('nav(demo)', function(elem){
-        $(".iframe-box > iframe").attr("src", elem.attr("data-url"));
-      });
+        // 监听左侧导航点击
+        element.on('nav(demo)', function(elem) {
+            $(".iframe-box > iframe").attr("src", elem.attr("data-url"));
+        });
+    });
+    /**
+     * 初始化教师的头像及姓名
+     */
+    function initTeacherInfo(tid){
+        $.ajax({
+            type : "post",
+            url : "teacher/teacherInit",
+            data : {
+            	teacherId: tid
+            },
+            dataType : "json",
+            success : function(result) {
+                var data = result.data;
+                $(".teacher-img").attr("src", data.img);
+                $(".teacher-name").text(data.name);
+            }
+        });
+    }
 
+    // 退出登录
+    $(".logout").on("click", function() {
+        layui.use('layer', function() {
+            var layer = layui.layer;
+            layer.confirm('真滴退出登录?', {
+                icon : 3,
+                title : '提示'
+            }, function(index) {
+                $.ajax({
+                    type : "post",
+                    url : "teacher/logout",
+                    data : {},
+                    dataType : "json",
+                    success : function(result) {
+                        layer.msg(result.msg);
+                        // 注销成功,跳转到登录页面
+                        window.location = 'login.html';
+                    }
+                });
+            });
+        });
     });
 });
