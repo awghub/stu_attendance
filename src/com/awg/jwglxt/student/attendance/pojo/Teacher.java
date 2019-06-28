060c0586327f15d8ab12d6fa9eeaf9efe5d62e8c
IAMAWG <m15528265897@163.com>
2019-06-28 15:40:18 +0800

    新增教师（Teacher）实体对象

---
+++ src/com/awg/jwglxt/student/attendance/pojo/Teacher.java
@@ -0,0 +1,91 @@
+package com.awg.jwglxt.student.attendance.pojo;
+
+import java.io.Serializable;
+
+public class Teacher implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer teacherId;
+	private String tName;
+	private Integer tGender;
+    private String tTel;
+	private String address;
+	private Integer type;
+	private Grade grade;
+	public Teacher(Integer teacherId, String tName, Integer tGender, String tTel, String address, Integer type,
+			Grade grade) {
+		super();
+		this.teacherId = teacherId;
+		this.tName = tName;
+		this.tGender = tGender;
+		this.tTel = tTel;
+		this.address = address;
+		this.type = type;
+		this.grade = grade;
+	}
+	public Teacher() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Teacher(String tName) {
+		super();
+		this.tName = tName;
+	}
+	public Teacher(String tName, Integer tGender, String tTel, String address) {
+		super();
+		this.tName = tName;
+		this.tGender = tGender;
+		this.tTel = tTel;
+		this.address = address;
+	}
+	public Integer getTeacherId() {
+		return teacherId;
+	}
+	public void setTeacherId(Integer teacherId) {
+		this.teacherId = teacherId;
+	}
+	public String gettName() {
+		return tName;
+	}
+	public void settName(String tName) {
+		this.tName = tName;
+	}
+	public Integer gettGender() {
+		return tGender;
+	}
+	public void settGender(Integer tGender) {
+		this.tGender = tGender;
+	}
+	public String gettTel() {
+		return tTel;
+	}
+	public void settTel(String tTel) {
+		this.tTel = tTel;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Integer getType() {
+		return type;
+	}
+	public void setType(Integer type) {
+		this.type = type;
+	}
+	public Grade getGrade() {
+		return grade;
+	}
+	public void setGrade(Grade grade) {
+		this.grade = grade;
+	}
+	@Override
+	public String toString() {
+		return "Teacher [teacherId=" + teacherId + ", tName=" + tName + ", tGender=" + tGender + ", tTel=" + tTel
+				+ ", address=" + address + ", type=" + type + ", grade=" + grade + "]";
+	}
+	
+	
+}
